093e6dfe4cc0d93c7e04667be86985040b9ac768
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-04-11 15:19:09 -0300

    Testing the app state

--- FitNess/FitNess/App Model/AppModel.swift
+++ FitNess/FitNess/App Model/AppModel.swift
@@ -31,5 +31,11 @@ public class AppModel {
 
   static let instance = AppModel()
 
+  public var appState: AppState = .notStarted
+
   public init() {}
+
+  public func start() {
+    appState = .inProgress
+  }
 }
